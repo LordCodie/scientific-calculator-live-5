f30f449a4e90ff9c5587b906c226a23220382886
LordCodie <153275673+LordCodie@users.noreply.github.com>
2024-02-04 15:21:41 +0000

    Implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -1,39 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const display = document.getElementById('calc-display')
-    const buttons = document.getElementsByClassName('btn')
+  const display = document.getElementById('calc-display')
+  const buttons = document.getElementsByClassName('btn')
 
-    let currentValue = '';
+  let currentValue = '';
 
-    function evaluateResult() {
-      const convertedValue = currentValue
-        .replace('×', '*')
-        .replace('÷', '/')
-        .replace('%', '*0.01')
+  function evaluateResult() {
+    const convertedValue = currentValue
+      .replace('×', '*')
+      .replace('÷', '/')
+      .replace('%', '*0.01')
+      .replace('sin', 'Math.sin')
+      .replace('ln', 'Math.log')
+      .replace('π', 'Math.PI')
+      .replace('cos', 'Math.cos')
+      .replace('log', 'Math.log10')
+      .replace('e', 'Math.e')
+      .replace('tan', 'Math.tan')
+      .replace('√', 'Math.sqrt')
+
+    const result = eval(convertedValue);
+    currentValue = result.toString();
+    display.value = currentValue;
+  }
+
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', function () {
+      const value = button.innerText;
+
+      if (value == 'AC') {
+        currentValue = '';
+        display.value = currentValue;
+      } else if (value == '=') {
+        evaluateResult();
+      } else {
+        currentValue += value;
+        display.value = currentValue;
+      }
+
+    })
+
+  }
 
 
-      const result = eval(convertedValue);
-      currentValue = result.toString();
-      display.value = currentValue;
-    }
-
-    for (let i = 0; i < buttons.length; i++) {
-      const button = buttons[i];
-      button.addEventListener('click', function () {
-        const value = button.innerText;
-
-        if (value == 'AC') {
-          currentValue = '';
-          display.value = currentValue;
-        } else if (value == '=') {
-          evaluateResult();
-        } else {
-          currentValue += value;
-          display.value = currentValue;
-        }
-
-      })
-
-    }
-
-
-  });
+});
